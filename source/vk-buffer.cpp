9be33d1f70ce681321c1f00fb657057595e9295b
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2021-12-16 11:19:09 -0300

    refactor: vk::buffer::create is templated

--- source/vk-buffer.cpp
+++ source/vk-buffer.cpp
@@ -5,25 +5,6 @@
 
 namespace vk {
 namespace buffer {
-    VkBuffer
-    create(const VkDevice& device, uint64_t size, uint32_t family) {
-        VkBuffer buffer;
-
-        VkBufferCreateInfo info{};
-        info.sType                  = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-        info.size                   = sizeof(uint8_t) * size;
-        info.usage                  = VK_BUFFER_USAGE_STORAGE_BUFFER_BIT;
-        info.sharingMode            = VK_SHARING_MODE_EXCLUSIVE;
-        info.queueFamilyIndexCount  = 1;
-        info.pQueueFamilyIndices    = &family;
-
-        VkResult result = vkCreateBuffer(device, &info, nullptr, &buffer);
-        if (VK_SUCCESS != result) {
-            throw std::runtime_error("failed to create buffers");
-        }
-        return buffer;
-    }
-
     void
     destroy(const VkDevice& device, VkBuffer buffer) {
         vkDestroyBuffer(device, buffer, nullptr);
